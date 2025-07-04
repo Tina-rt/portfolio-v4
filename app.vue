b23ec4187ad69319792b2e52deddd8c25b506ed4
Tina Ratolojanahary <rtinahubert@gamil.com>
2025-07-04 15:21:03 +0300

    fix: responsive on mobile and desktop

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="w-[600px] m-auto pt-2">
-
+  <div class="main">
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.main {
+  width: 100%;
+  padding-inline: 1rem;
+}
+
+@media (min-width: 1000px) {
+  .main {
+    width: 600px;
+    margin: auto;
+    padding: 0;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-10">
+  <div class="flex flex-col gap-10 p-2">
 
     <div>
       <Navbar />
