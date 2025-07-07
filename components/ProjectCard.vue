9d5a3a15de94763a9590b4bbdffd35050f09911c
Tina Ratolojanahary <rtinahubert@gamil.com>
2025-07-07 20:24:01 +0300

    ajust project alignement content

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="card bg-base-200 w-full shadow-sm">
 
-    <div class="card-body flex flex-row justify-between">
-      <div>
+    <div class="card-body flex flex-row items-center justify-between">
+      <div class="h-full flex flex-col justify-between">
         <h2 class="card-title">{{ props.title }}</h2>
         <p>{{ props.description }}</p>
-        <div class="flex gap-1 flex-wrap my-2 pt-4">
-          <div class="badge badge-soft" v-for="b in props.skills">{{ b }}</div>
-        </div>
+
         <div class="card-actions ">
           <button class="btn btn-primary">View </button>
+        </div>
+        <div class="flex gap-1 flex-wrap my-2 pt-4">
+          <div class="badge badge-soft" v-for="b in props.skills">{{ b }}</div>
         </div>
       </div>
       <figure class="w-1/3 rounded-2xl">
